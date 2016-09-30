cb7a8c305972dfb15c0cdf21ebd074c096041bd2
sanjibc <sannjibc@cybage.com>
2016-09-30 15:47:59 +0530

    all raised issue are fixed

---
+++ app/code/Cybage/Quotation/Block/Addtoquote.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Cybage Quotation Plugin
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * It is available on the World Wide Web at:
+ * htt
+  /**p://opensource.org/licenses/osl-3.0.php
+ * If you are unable to access it on the World Wide Web, please send an email
+ * To: Support_Magento@cybage.com.  We will send you a copy of the source file.
+ *
+ * @category   Quotation Plugin
+ * @package    Cybage_Quotation
+ * @copyright  Copyright (c) 2014 Cybage Software Pvt. Ltd., India
+ *             http://www.cybage.com/pages/centers-of-excellence/ecommerce/ecommerce.aspx
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author     Cybage Software Pvt. Ltd. <Support_Magento@cybage.com>
+ */
+
+namespace Cybage\Quotation\Block;
+
+class Addtoquote extends \Magento\Framework\View\Element\Template
+{
+    //put your code here
+    public function __construct(\Magento\Framework\View\Element\Template\Context $context, array $data = array())
+    {
+        echo 'working';
+        parent::__construct($context, $data);
+    }
+	
+	public function test(){
+		echo 'testing';
+	}
+}
--- app/code/Cybage/Quotation/Block/Adminhtml/Quotation/Edit.php
+++ app/code/Cybage/Quotation/Block/Adminhtml/Quotation/Edit.php
@@ -1,14 +1,17 @@
 <?php
+
 namespace Cybage\Quotation\Block\Adminhtml\Quotation;
 
 class Edit extends \Magento\Backend\Block\Widget\Form\Container
 {
+
     /**
      * Core registry
      *
      * @var \Magento\Framework\Registry
      */
     protected $_coreRegistry = null;
+
     /**
      * @param \Magento\Backend\Block\Widget\Context $context
      * @param \Magento\Framework\Registry $registry
@@ -19,6 +22,7 @@ class Edit extends \Magento\Backend\Block\Widget\Form\Container
         $this->_coreRegistry = $registry;
         parent::__construct($context, $data);
     }
+
     /**
      * Initialize blog post edit block
      *
@@ -32,9 +36,18 @@ class Edit extends \Magento\Backend\Block\Widget\Form\Container
         parent::_construct();
         $this->buttonList->remove('delete');
         $this->buttonList->remove('reset');
+        $this->buttonList->remove('back');
         $this->buttonList->update('save', 'label', __('Save Quotation'));
 
+        $this->addButton(
+                'my_back_button', [
+            'label' => __('Back'),
+            'onclick' => 'setLocation(\'' . $this->getUrl('quotation/listquotation/index') . '\')',
+            'class' => 'back'
+                ], -1
+        );
     }
+
     /**
      * Retrieve text for header element depending on loaded post
      *
@@ -48,6 +61,7 @@ class Edit extends \Magento\Backend\Block\Widget\Form\Container
             return __('New Post');
         }
     }
+
     /**
      * Check permission for passed action
      *
@@ -58,6 +72,7 @@ class Edit extends \Magento\Backend\Block\Widget\Form\Container
     {
         return $this->_authorization->isAllowed($resourceId);
     }
+
     /**
      * Getter of url for "Save and Continue" button
      * tab_id will be replaced by desired by JS later
@@ -68,4 +83,5 @@ class Edit extends \Magento\Backend\Block\Widget\Form\Container
     {
         return $this->getUrl('blog/*/save', ['_current' => true, 'back' => 'edit', 'active_tab' => '']);
     }
+
 }
